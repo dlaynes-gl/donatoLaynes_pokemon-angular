c781375be1d1cd7843aa1c29e7772636513c592e
dlaynes-gl <88404240+dlaynes-gl@users.noreply.github.com>
2022-09-19 08:21:51 -0500

    Add libraries

--- apps/pokemon/src/app/app.module.ts
+++ apps/pokemon/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
