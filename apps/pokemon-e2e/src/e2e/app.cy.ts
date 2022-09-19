bce4a1dc50241a33008f901f9ddde7504e55d4d3
dlaynes-gl <88404240+dlaynes-gl@users.noreply.github.com>
2022-09-19 17:05:04 -0500

    Improve Listing test

--- apps/pokemon-e2e/src/e2e/app.cy.ts
+++ apps/pokemon-e2e/src/e2e/app.cy.ts
@@ -1,5 +1,6 @@
 import {
   getPokemonList,
+  getFirstPokemonFromList,
   getPreviousButton,
   getNextButton,
   getSearchInput,
@@ -10,11 +11,26 @@ import {
   getPokemonDetailWeight,
 } from '../support/app.po';
 
+const baseUrl = 'https://pokeapi.co/api/v2/'
+
 describe('pokemon', () => {
-  beforeEach(() => cy.visit('/'));
+  before(() => {
+    cy.fixture('pokemonList').as('pokemonList')
+    cy.fixture('pokemon/bulbasaur').as('bulbasaur')
+    cy.fixture('pokemon/ivysaur').as('ivysaur')
+    cy.fixture('pokemon/venusaur').as('venusaur')
+    cy.fixture('pokemon/charmander').as('charmander')
+    cy.fixture('pokemon/charmeleon').as('charmeleon')
+    cy.fixture('pokemon/charizard').as('charizard')
+    cy.fixture('pokemon/squirtle').as('squirtle')
+  });
 
   it('should list 4 pokemon by default', () => {
     getPokemonList().should((t) => expect(t.length).equal(4));
+  });
+
+  it('should show detailed information of each pokemon in the dashboard page', () => {
+    getFirstPokemonFromList().should('have.data')
   });
 
   it('should navigate to the next page', () => {
